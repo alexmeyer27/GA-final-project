d2213177e0215e0addefc7d2f6881dd604341178
Alex Meyer <alexmeyer27@gmail.com>
2018-09-20 22:46:07 -0600

    api call

--- app.js
+++ app.js
@@ -34,3 +34,103 @@ recipeSearch("pasta");
 
 //recipes are returned sorted alphabetically
 
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+// DOM elements
+const button = document.querySelector('.btn'),
+      form = document.getElementById('message-form'),
+      ul = document.querySelector('.message-board');
+
+// initialize userMessage
+let userMessage = "";
+
+form.addEventListener('submit', function() {
+  const messageBox = document.getElementById('message');
+  
+  userMessage = messageBox.value;
+  event.preventDefault();
+
+  var dbReference = firebase.database().ref('messages');
+  
+  dbReference.push({
+    message: userMessage,
+    votes: 0
+  });
+  
+  messageBox.value = '';
+  
+});
+
+
+database.ref('messages').on('value', function (results) {
+  ul.innerHTML = '';
+  let allMessages = results.val();
+  
+  for(var messageID in allMessages){
+    //console.log(messageID);
+    //console.log(allMessages[messageID].message);
+    
+    let li = document.createElement('li'),
+        boardMessage = allMessages[messageID].message,
+        trashIcon = `<i class="fa fa-trash pull-right delete"></i>`,
+        editIcon = `<i class="fa fa-pencil pull-right edit" data-toggle="modal" data-target="#editModal"></i>`; 
+    
+    li.innerHTML = `${boardMessage} ${trashIcon} ${editIcon}`;
+    li.setAttribute('data-id', messageID);
+    
+    ul.appendChild(li);
+  }
+  deletr();
+  editr();
+});
+
+function deletr() {
+  const deleteMe = document.querySelectorAll('.delete');
+  //console.log(deleteMe);
+  deleteMe.forEach(element => {
+    element.addEventListener('click', function() {
+      let removeLi = this.parentNode,
+          dataID = removeLi.getAttribute('data-id'),
+          messageReference =  database.ref('messages/' + dataID);
+      
+      messageReference.remove();     
+    });
+  }); 
+}
+
+function editr() {
+  const editMe = document.querySelectorAll('.edit');
+  
+  editMe.forEach(element => {
+   element.addEventListener('click', function() {
+     let updateLi = this.parentNode,
+          dataID = updateLi.getAttribute('data-id'),
+          messageReference =  database.ref('messages/' + dataID);
+     
+     const updateMessageForm = document.getElementById('edit-message');
+     
+     updateMessageForm.addEventListener('submit', function() {
+       
+       
+       
+       let newMessageBox = document.getElementById('updatedMessage'),
+           updatedMessage = newMessageBox.value,
+           modalBox = document.getElementById('editModal');
+       
+       event.preventDefault();
+       messageReference.update({
+         message: updatedMessage
+       });
+       
+       //modalBox.modal('hide');
+       newMessageBox.value = '';
+       ul.innerHTML = '';
+     });
+     
+   });
+  });
+}
+
