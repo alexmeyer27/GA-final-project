d02e77a313e719c136ed7f2ea114043837fc4908
Alex Meyer <alexmeyer27@gmail.com>
2018-09-22 15:27:34 -0600

    major functionality additions

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+//variable
+const searchBar = $('#recipe-search'),
+      button = $('#submit-button'),
+      defaultBookmark = $('.far fa-bookmark');
+    
+
+// Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBaNtqY99qwGw8UtGlCOkrFrBZNqbO9IWA",
+    authDomain: "recipe-project-d634f.firebaseapp.com",
+    databaseURL: "https://recipe-project-d634f.firebaseio.com",
+    projectId: "recipe-project-d634f",
+    storageBucket: "recipe-project-d634f.appspot.com",
+    messagingSenderId: "556672499012"
+  };
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+var recipeDatabaseReference = firebase.database().ref('recipes');
+
+
 // API call using jquery AJAX, promises, functions created in global scope
-
-
 var recipeSearch = function(searchTerm){
 	//jquery ajax call
 	$.ajax({
@@ -10,7 +31,34 @@ var recipeSearch = function(searchTerm){
     	jsonp: false,
 		url: `https://www.food2fork.com/api/search?key=5e10831907c423a1bf607fe78627c48b&q=${searchTerm}`, 
 	 	success: function(result) {
-	 		console.log(result);
+			event.preventDefault();
+			console.log(result);
+			var i;
+		////success function creates html and inserts as html (to be created)
+		for (i = 0; i < result.recipes.length; i++){
+
+			var recipe = result.recipes[i];
+
+			var	recipeTitle = recipe.title,
+				author = recipe.publisher,
+				recipeURL = recipe.source_url,
+				imageURL = recipe.image_url;
+				
+			//defining article template
+			//ToDo: refactor
+			var recipeTemplate = 
+			"<div class = 'recipe'><h3>" + recipeTitle + "</h3>" + 
+			"<h5>" + author + "</h5>" 
+			+ "<a ref = " + recipeURL + ">Source</a>" 
+			+ "<img src = " + imageURL + ">" 
+			+ "<i class='far fa-bookmark' onclick = 'bookmark(this)'></i></div>" 
+
+				
+			//appending each article after the search bar
+			$("#recipe_search_form").after(recipeTemplate);
+				
+			}
+		i++
 	 	},
 	 	error: function() {
 			alert("Error - api call unsuccessful. Check console");
@@ -18,16 +66,62 @@ var recipeSearch = function(searchTerm){
 	})
 }
 
-recipeSearch("pasta");
 
-//success function creates object and inserts as html (to be created)
+//submit function to call API
+button.click( function (){
+	event.preventDefault();
+	submitValue = searchBar.val();
+	recipeSearch(submitValue);
+});
+
 
 
 //link to bookmark button that adds to firebase database
+function bookmark(bookmarkElement) { 
+   
+	//add parent elements to database as key:value pairs
+
+	var parentElementHelper = $(event.target).parent();
+
+	var recipeTitle = $(parentElementHelper > "h3").val(),
+		recipeURL = $(parentElementHelper > "a").val();
+
+	var recipeHTML = `<a href=${recipeURL}>${recipeTitle}</a>`
+
+	recipeDatabaseReference.push({
+		recipeHTML: recipeHTML
+	})
+
+	//change bookmark icon
+    $(event.target).after('<i class="fas fa-bookmark"></i>');
+    bookmarkElement.remove();
+
+}
 
 
 //ability to manually add recipe by url (less information when adding this way)
+function addRecipeByURL(recipeTitle) {
 
+	//input element variables 
+	var recipeTitleInput = $("#recipe-add-title"),
+		recipeURLInput = $("#recipe-add-url"),
+		manualAddButton = $("#add-button");
+
+	//data variables
+	var recipeTitle = recipeTitleInput.val(),
+		recipeURL = recipeURLInput.val();
+
+	var recipeHTML = `<a href=${recipeURL}>${recipeTitle}</a>`
+
+	//event listener for button/input
+	manualAddButton.click( function (){
+		event.preventDefault();
+		recipeHTML = `<a href=${recipeURL}>${recipeTitle}</a>`
+		recipeDatabaseReference.push({
+			recipeHTML: recipeHTML
+		})
+	});
+}
 
 //selecting recipe book expands section of the page to show bookmarked recipes
 
@@ -35,102 +129,5 @@ recipeSearch("pasta");
 //recipes are returned sorted alphabetically
 
 
-firebase.initializeApp(config);
 
-const database = firebase.database();
-
-// DOM elements
-const button = document.querySelector('.btn'),
-      form = document.getElementById('message-form'),
-      ul = document.querySelector('.message-board');
-
-// initialize userMessage
-let userMessage = "";
-
-form.addEventListener('submit', function() {
-  const messageBox = document.getElementById('message');
-  
-  userMessage = messageBox.value;
-  event.preventDefault();
-
-  var dbReference = firebase.database().ref('messages');
-  
-  dbReference.push({
-    message: userMessage,
-    votes: 0
-  });
-  
-  messageBox.value = '';
-  
-});
-
-
-database.ref('messages').on('value', function (results) {
-  ul.innerHTML = '';
-  let allMessages = results.val();
-  
-  for(var messageID in allMessages){
-    //console.log(messageID);
-    //console.log(allMessages[messageID].message);
-    
-    let li = document.createElement('li'),
-        boardMessage = allMessages[messageID].message,
-        trashIcon = `<i class="fa fa-trash pull-right delete"></i>`,
-        editIcon = `<i class="fa fa-pencil pull-right edit" data-toggle="modal" data-target="#editModal"></i>`; 
-    
-    li.innerHTML = `${boardMessage} ${trashIcon} ${editIcon}`;
-    li.setAttribute('data-id', messageID);
-    
-    ul.appendChild(li);
-  }
-  deletr();
-  editr();
-});
-
-function deletr() {
-  const deleteMe = document.querySelectorAll('.delete');
-  //console.log(deleteMe);
-  deleteMe.forEach(element => {
-    element.addEventListener('click', function() {
-      let removeLi = this.parentNode,
-          dataID = removeLi.getAttribute('data-id'),
-          messageReference =  database.ref('messages/' + dataID);
-      
-      messageReference.remove();     
-    });
-  }); 
-}
-
-function editr() {
-  const editMe = document.querySelectorAll('.edit');
-  
-  editMe.forEach(element => {
-   element.addEventListener('click', function() {
-     let updateLi = this.parentNode,
-          dataID = updateLi.getAttribute('data-id'),
-          messageReference =  database.ref('messages/' + dataID);
-     
-     const updateMessageForm = document.getElementById('edit-message');
-     
-     updateMessageForm.addEventListener('submit', function() {
-       
-       
-       
-       let newMessageBox = document.getElementById('updatedMessage'),
-           updatedMessage = newMessageBox.value,
-           modalBox = document.getElementById('editModal');
-       
-       event.preventDefault();
-       messageReference.update({
-         message: updatedMessage
-       });
-       
-       //modalBox.modal('hide');
-       newMessageBox.value = '';
-       ul.innerHTML = '';
-     });
-     
-   });
-  });
-}
 
